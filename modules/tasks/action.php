1b9f41dae9afd996db7344d5d0594ce77977aa36
matges <github.com@weltenspiegel.de>
2024-10-23 11:25:52 +0000

    empty users fix
    git-svn-id: https://192.168.222.2/svn/buchen.mit-sinn-leben.de@57 342be62c-e02d-4a83-ae5b-65797dcb4769

--- modules/tasks/action.php
+++ modules/tasks/action.php
@@ -30,8 +30,11 @@ function execute_assign(){
     $task->users=$sorted;
   }
   #logger(print_r($tasks,1));
-  require_once('users.class.php');
-  $users = new Users(array('id' => array_keys($user_ids)));
+  $users=array();
+  if(!empty($user_ids)){
+    require_once('users.class.php');
+    $users = new Users(array('id' => array_keys($user_ids)));
+  }
   #logger(print_r($users,1));
   return array('tasks'=>$tasks, 'users'=>$users);
 }
