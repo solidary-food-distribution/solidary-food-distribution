a08a4ef9f9144e77e16b560c8d41bd4e5a48f4aa
matges <github.com@weltenspiegel.de>
2025-04-11 13:16:04 +0000

    deliveries with date select
    git-svn-id: https://192.168.222.2/svn/buchen.mit-sinn-leben.de@127 342be62c-e02d-4a83-ae5b-65797dcb4769

--- modules/deliveries/action.php
+++ modules/deliveries/action.php
@@ -5,13 +5,44 @@ user_ensure_authed();
 user_needs_access('deliveries');
 
 function execute_index(){
+  $date = get_request_param('date');
+
   require_once('deliveries.class.php');
-  $ds = new Deliveries(array(), array('id' => 'DESC') , 0, 6);
+  $date_prev = '';
+  if($date == ''){
+    $ds = new Deliveries(array('created<=' => date('Y-m-d')), array('created' => 'DESC'), 0, 10);
+    foreach($ds as $d){
+      if($date == ''){
+        $date = substr($d->created, 0, 10);
+      }
+      if($date_prev == '' && substr($d->created, 0, 10) < $date){
+        $date_prev = substr($d->created, 0, 10);
+        break;
+      }
+    }
+  }
+
+  if($date_prev == ''){
+    $ds = new Deliveries(array('created<' => $date), array('created' => 'DESC'), 0, 1);
+    if($ds->count()){
+      $date_prev = substr($ds->first()->created, 0, 10);
+    }
+  }
+
+  $date_next = '';
+  $ds = new Deliveries(array('created>' => $date.' 23:59:59'), array('created' => 'ASC'), 0, 1);
+  if($ds->count()){
+    $date_next = substr($ds->first()->created, 0, 10);
+  }
+
   $deliveries = array();
+  $ds = new Deliveries(array('created>=' => $date), array('created' => 'ASC', 'supplier_id' => 'ASC') , 0, 10);
   foreach($ds as $id => $d){
+    if(substr($d->created,0, 10) > $date){
+      break;
+    }
     $deliveries[$id] = $d;
   }
-  $deliveries = array_reverse($deliveries, true);
 
   require_once('delivery_items.class.php');
   $dis = new DeliveryItems(array('delivery_id' => array_keys($deliveries)));
@@ -29,5 +60,5 @@ function execute_index(){
   require_once('members.class.php');
   $suppliers = new Members(array('producer' => array(1,2)));
 
-  return array('deliveries'=>$deliveries, 'delivery_items' => $delivery_items, 'suppliers' => $suppliers, 'products' => $products);
+  return array('date' => $date, 'date_prev' => $date_prev, 'date_next' => $date_next, 'deliveries'=>$deliveries, 'delivery_items' => $delivery_items, 'suppliers' => $suppliers, 'products' => $products);
 }
--- modules/deliveries/templates/index.php
+++ modules/deliveries/templates/index.php
@@ -4,9 +4,15 @@ $PROPERTIES['body_class']='header_h5';
 ?>
 
 <?php ob_start(); ?>
-  <div class="controls">
-
+<div class="controls">
+  <div class="control_l input <?php echo $date_prev?'':'disabled' ?>" onclick="<?php echo $date_prev?'location.href=\'/deliveries?date='.$date_prev.'\'':'' ?>">
+    <i class="fa-solid fa-caret-left"></i>
+  </div><div class="control_m input">
+    <?php echo format_date($date); ?>
+  </div><div class="control_r input <?php echo $date_next?'':'disabled' ?>" onclick="<?php echo $date_next?'location.href=\'/deliveries?date='.$date_next.'\'':'' ?>">
+    <i class="fa-solid fa-caret-right"></i>
   </div>
+</div>
 <?php $PROPERTIES['header']=ob_get_clean(); ?>
 
 <?php foreach($deliveries as $delivery): ?>
@@ -14,8 +20,6 @@ $PROPERTIES['body_class']='header_h5';
     <div class="col4">
       <div>
         <b><?php echo format_date($delivery->created,'j.n.Y') ?></b>
-        <br>
-        <?php echo format_date($delivery->created,'H:i', false) ?>
       </div>
     </div>
     <div class="col3">
